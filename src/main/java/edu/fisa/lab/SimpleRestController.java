f768500b621a64eae79294205df7acbed04046e4
yeomyaloo <yeom456@gmail.com>
2024-02-20 15:00:57 +0900

    first commit

---
+++ src/main/java/edu/fisa/lab/SimpleRestController.java
@@ -0,0 +1,13 @@
+package edu.fisa.lab;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleRestController {
+
+	@GetMapping("/getdata")
+	public String getData() {
+		return "메롱";
+	}
+}
